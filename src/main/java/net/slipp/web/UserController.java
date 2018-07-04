f5b1958decd57c41c310640ae960bcd4b1b1debd
DaeHoan Shin <xbb123@naver.com>
2018-07-05 00:57:04 +0900

    3-5 put complete

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.slipp.domain.User;
@@ -41,7 +42,7 @@ public class UserController {
 		return "/user/updateForm";
 	}
 	
-	@PostMapping("{id}")
+	@PutMapping("{id}")
 	public String update(@PathVariable Long id, User newUser) {
 		User user = UserRepository.findOne(id);
 		user.update(newUser);
