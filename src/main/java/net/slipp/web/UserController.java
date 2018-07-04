64f45b2f655d631ec20a940c61dbcdcc0fbe8666
DaeHoan Shin <xbb123@naver.com>
2018-07-05 00:36:54 +0900

    3-4 complete

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -4,27 +4,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.slipp.domain.User;
 import net.slipp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	
 	@Autowired
 	private UserRepository UserRepository;
 	
-	@PostMapping("/user/create")
+	@GetMapping("/form")
+	public String form() {
+		return "/user/form";
+	}
+	@PostMapping("")
 	public String create(User user) {
 		System.out.println("user : " + user);
 		UserRepository.save(user);
-		return "redirect:/user/list";
+		return "redirect:/users";
 	}
 	
-	@GetMapping("/user/list")
+	@GetMapping("")
 	public String list(Model model) {
 		model.addAttribute("users", UserRepository.findAll());
 		return "/user/list";
+	}
+	@GetMapping("{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		User user = UserRepository.findOne(id);
+		model.addAttribute("user", user);
+		return "/user/updateForm";
 	}
 }
