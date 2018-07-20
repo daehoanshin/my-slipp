1a85cbf0edff0412382774bbaa9692ba544a7f69
DaeHoan Shin <xbb123@naver.com>
2018-07-20 10:23:52 +0900

    6-5 중복제거및 리펙토링

--- src/main/java/net/slipp/domain/User.java
+++ src/main/java/net/slipp/domain/User.java
@@ -9,12 +9,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
-public class User {
-	@Id
-	@GeneratedValue
-	@JsonProperty
-	private Long id;
-	
+public class User extends AbstractEntity {
+
 	@Column(nullable=false, length=20, unique=true)
 	@JsonProperty
 	private String userId;
@@ -27,12 +23,7 @@ public class User {
 	@JsonProperty
 	private String email;
 
-	public boolean matchId(Long newId) {
-		if(newId == null) {
-			return false;
-		}
-		return newId.equals(id);
-	}
+	
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
@@ -64,33 +55,18 @@ public class User {
 		this.name = newUser.name;
 		this.email = newUser.email;
 	}
+	
+	public boolean matchId(Long newId) {
+		if(newId == null) {
+			return false;
+		}
+		return newId.equals(getId());
+	}
+
 	@Override
 	public String toString() {
-		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
-	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
+		return "User [" + super.toString() + ", userId=" + userId + ", password=" + password + ", name=" + name + ", email="
+				+ email + "]";
 	}
 	
 }
